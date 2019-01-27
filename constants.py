860fdca84b141bb37cf1de87e031fd761525fb49
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-01-27 17:54:15 +0530

    create file for constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+USDT = "&pref=USDT"
+USD = "&pref=USD"
+BTC = "&pref=BTC"
+EUR = "&pref=EUR"
+JPY = "&pref=JPY"
+INR = "&pref=INR"
+AUD = "&pref=AUD"
+GBP = "&pref=GBP"
+
--- lookup.py
+++ lookup.py
@@ -2,6 +2,8 @@ import requests
 import json
 import os
 
+import constants
+
 os.system('clear')
 
 user_api = open("API", "r")
@@ -9,42 +11,30 @@ api_key = user_api.read().rstrip()
 user_api.close()
 print(api_key+"\n-------\n")
 
-usd = "&pref=USD"
-btc = "&pref=BTC"
-
-def human_format(num):
-    if type(num) is str:
-        num = int(num[:-11])
-    num = float('{:.3g}'.format(num))
-    magnitude = 0
-    while abs(num) >= 1000:
-        magnitude += 1
-        num /= 1000.0
-    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
-
 def human_format(num, round_to=2):
     if type(num) is str:
-        num = int(num[:-11])
+        num = int(num[:-11])  # Remove decimals
     magnitude = 0
     while abs(num) >= 1000:
         magnitude += 1
         num = round(num / 1000.0, round_to)
-    return '{:.{}f}{}'.format(round(num, round_to), round_to, ['', 'K', 'M', 'B', 'T', 'P'][magnitude])
+    return '{:.{}f}{}'.format(round(num, round_to), round_to, ['', 'K', 'M', 'B', 'T'][magnitude])
 
-coinlib_global = "https://coinlib.io/api/v1/global?key=" + api_key + usd
+coinlib_global = "https://coinlib.io/api/v1/global?key=" + api_key + constants.USD
 print(coinlib_global,"\n-----\n")
 api_requests = requests.get(coinlib_global)
 global_market = json.loads(api_requests.content)
 print(global_market, "\n------\n")
 
+
+#TODO modify coinlib_global object
+
+
 if global_market['remaining'] > 10:
     print("Total Market Cap : ",global_market['total_market_cap'], "\n")
     print(human_format(global_market['total_market_cap']), "\n")
-    print("{:.3}".format(global_market['total_market_cap'])+"B","\n")
-    print("Total Market Cap : ", global_market['total_market_cap'][:-4], "\n")
     print("Total 24H Volume : ",global_market['total_volume_24h'], "\n")
     print(human_format(global_market['total_volume_24h']), "\n")
-    print(human_format(global_market['total_volume_24h'],3), "\n")
 
 """
 coinlist = "https://coinlib.io/api/v1/coinlist?key=" + api_key + usd + "&page=1"
