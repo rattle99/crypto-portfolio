ae1cb465f106f478db74178a687a2b00663df975
rattle99 <24495512+rattle99@users.noreply.github.com>
2019-01-29 21:29:27 +0530

    format global market object

--- lookup.py
+++ lookup.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from datetime import datetime
 
 import constants
 
@@ -9,7 +10,7 @@ os.system('clear')
 user_api = open("API", "r")
 api_key = user_api.read().rstrip()
 user_api.close()
-print(api_key+"\n-------\n")
+
 
 def human_format(num, round_to=2):
     if type(num) is str:
@@ -20,21 +21,28 @@ def human_format(num, round_to=2):
         num = round(num / 1000.0, round_to)
     return '{:.{}f}{}'.format(round(num, round_to), round_to, ['', 'K', 'M', 'B', 'T'][magnitude])
 
+
 coinlib_global = "https://coinlib.io/api/v1/global?key=" + api_key + constants.USD
-print(coinlib_global,"\n-----\n")
+print("From ", coinlib_global, "\n-----\n")
 api_requests = requests.get(coinlib_global)
 global_market = json.loads(api_requests.content)
-print(global_market, "\n------\n")
 
-
-#TODO modify coinlib_global object
-
+# Format global market data
+global_market.pop('coins')
+global_market.pop('markets')
+global_market['total_market_cap'] = human_format(global_market['total_market_cap'])
+global_market['total_volume_24h'] = human_format(global_market['total_volume_24h'])
+global_market['last_updated_timestamp'] = \
+    datetime.utcfromtimestamp(global_market['last_updated_timestamp']).strftime('%Y-%m-%d %H:%M:%S')
 
 if global_market['remaining'] > 10:
-    print("Total Market Cap : ",global_market['total_market_cap'], "\n")
-    print(human_format(global_market['total_market_cap']), "\n")
-    print("Total 24H Volume : ",global_market['total_volume_24h'], "\n")
-    print(human_format(global_market['total_volume_24h']), "\n")
+    print("Total Market Cap : ", global_market['total_market_cap'], "\n")
+    print("Total 24H Volume : ", global_market['total_volume_24h'], "\n")
+    print("Last Updated : ", global_market['last_updated_timestamp'], "\n")
+    print("Remaining Calls : ", global_market['remaining'], "\n")
+
+print(global_market.keys())
+
 
 """
 coinlist = "https://coinlib.io/api/v1/coinlist?key=" + api_key + usd + "&page=1"
